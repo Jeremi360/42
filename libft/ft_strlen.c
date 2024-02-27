1a3a273614395612dd2c957d151c65e4303ea390
Jeremi Biernacki <jeremi360@gmail.com>
2024-02-27 13:27:28 +0100

    added strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeremi360 <jeremi360@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 11:54:57 by jeremi360         #+#    #+#             */
-/*   Updated: 2024/02/27 11:56:45 by jeremi360        ###   ########.fr       */
+/*   Created: 2024/02/27 13:23:05 by jeremi360         #+#    #+#             */
+/*   Updated: 2024/02/27 13:23:09 by jeremi360        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
-#endif
-
-/* FUNCTIONS */
 int	ft_strlen(char *str)
-;
-int	ft_isprint(int c)
-;
-int ft_isascii(int c);
-int ft_isalnum(int c);
-int ft_isalpha(int c);
-int ft_isdigit(int c);
+{
+	int	len;
 
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
