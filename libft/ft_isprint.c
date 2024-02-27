ff25a2e05e451eb1f90637fb37d740d288c43118
Jeremi Biernacki <jeremi360@gmail.com>
2024-02-27 13:20:24 +0100

    add ft_isprint

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeremi360 <jeremi360@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 11:54:57 by jeremi360         #+#    #+#             */
-/*   Updated: 2024/02/27 11:56:45 by jeremi360        ###   ########.fr       */
+/*   Created: 2024/02/27 13:15:22 by jeremi360         #+#    #+#             */
+/*   Updated: 2024/02/27 13:16:30 by jeremi360        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
-#endif
-
-/* FUNCTIONS */
 int	ft_isprint(int c)
-;
-int ft_isascii(int c);
-int ft_isalnum(int c);
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-
+{
+	return (c >= 32 && c <= 126);
+}
---
+++ libft/ft_isprint_test.c
@@ -0,0 +1,12 @@
+
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int c;
+	for (c = 1; c <= 127; ++c)
+		if (ft_isprint(c) != 0)
+			printf("%c ", c);
+	return (0);
+}
