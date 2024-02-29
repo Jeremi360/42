f8df02278e325c95a1841296310c2c0a17149198
Jeremi Biernacki <jeremi360@gmail.com>
2024-02-29 15:55:39 +0100

    add strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -9,3 +9,19 @@
 /*   Updated: 2024/02/29 15:23:34 by jeremi360        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
