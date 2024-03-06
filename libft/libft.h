06d72240b02e9557646f4d87e5fe36109652ed48
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-06 16:15:06 +0100

    ft_striteri works

--- libft/libft.h
+++ libft/libft.h
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 
 /* FUNCTIONS */
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
