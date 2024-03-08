44939d58eddc6b534e6063bf3786179769957ba4
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-08 16:11:32 +0100

    add lstsize

--- libft/test/ft_lstsize.c
+++ libft/test/ft_lstsize.c
@@ -1,17 +1,15 @@
 #include "../libft.h"
 #include <stdio.h>
 
-// main that tests ft_lstadd_back
+// main that tests ft_lstsize
 int	main(void)
 {
-	t_list *list = ft_lstnew("Hello");
+	t_list	*list;
+
+	list = ft_lstnew("Hello");
 	ft_lstadd_back(&list, ft_lstnew("World!"));
 	ft_lstadd_back(&list, ft_lstnew("How"));
 	ft_lstadd_back(&list, ft_lstnew("Are"));
 	ft_lstadd_back(&list, ft_lstnew("You?"));
-	while (list)
-	{
-		printf("%s\n", (char *)list->content);
-		list = list->next;
-	}
+	printf("List size: %d\n", ft_lstsize(list));
 }
--- libft/test/ft_strrchr.c
+++ libft/test/ft_strrchr.c
@@ -4,7 +4,6 @@
 // test strrchr
 int	main(void)
 {
-	printf("== %s ==\n", __func__);
 	char *str = "Hello World!";
 	char *ptr = ft_strrchr(str, 'o');
 	printf("str: %s\n", str);
