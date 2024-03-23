daf7505f52eb6c316c3338859d0636f6e206ebb1
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-23 17:46:40 +0100

    fix libft norm

--- libft/libft.h
+++ libft/libft.h
@@ -34,7 +34,8 @@ t_list				*ft_lstnew(void *content);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list				*ft_lstmap(t_list *lst,
+						void *(*f)(void *), void (*del)(void *));
 
 /* PART 2 */
 void				ft_putendl_fd(char *str, int fd);
@@ -58,7 +59,7 @@ void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
 int					ft_atoi(const char *str);
-char				*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char				*ft_strnstr(const char *hay, const char *x, size_t len);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strchr(const char *str, int c);
