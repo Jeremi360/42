838f6fa813432cfe22ca8de0b26f6b4925eb89bd
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-23 19:47:11 +0100

    fixed

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jeremi360 <jbiernac@student.42warsaw.pl    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 14:28:25 by jeremi360         #+#    #+#             */
-/*   Updated: 2024/03/06 13:58:52 by jeremi360        ###   ########.fr       */
+/*   Updated: 2024/03/23 19:46:49 by jeremi360        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strdup(const char *s1)
 	char	*str;
 	size_t	len;
 
-	len = ft_strlen(s1);
+	len = ft_strlen(s1) + 1;
 	str = ft_calloc(len, sizeof(char));
 	if (!str)
 		return (NULL);
