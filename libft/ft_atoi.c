785c9ce5d40ed5e4da617e49cdfee62574a618d3
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-23 19:38:23 +0100

    atoi wroks

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: jeremi360 <jbiernac@student.42warsaw.pl    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:56:52 by jeremi360         #+#    #+#             */
-/*   Updated: 2024/03/23 19:03:36 by jeremi360        ###   ########.fr       */
+/*   Updated: 2024/03/23 19:37:47 by jeremi360        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,20 @@ int	ft_atoi(const char *str)
 	i = 0;
 	sign = 1;
 	nbr = 0;
-	while (str[i] != '\0')
+	if (ft_strlen(str) == 0)
+		return (0);
+	while (is_space(str[i]))
+		i++;
+	if (str[i] == '+' && str[i + 1] != '-')
+		i++;
+	if (str[i] == '-')
 	{
-		if (ft_strchr("+-", str[i]))
-		{
-			if (str[i] == '-')
-			{
-				sign = -1;
-			}
-			if (is_space(str[i + 1]))
-				return (0);
-			if (ft_strchr("+-", str[i + 1]))
-				return (0);
-		}
-		else if (ft_isdigit(str[i]))
-		{
-			nbr = nbr * 10 + (str[i] - '0');
-			if (is_space(str[i + 1]))
-				return (sign * nbr);
-		}
-		else if (is_space(str[i]))
-		{
-			if (nbr != 0)
-				return (sign * nbr);
-		}
-		else
-			return (0);
+		sign = -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		nbr = nbr * 10 + (str[i] - '0');
 		i++;
 	}
 	return (sign * nbr);
