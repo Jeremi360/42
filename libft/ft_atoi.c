24ee66fbdb6d5216f1ea0beaeb06962220dd7962
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-23 19:04:02 +0100

    it works, but is too long

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,18 @@
 /*   By: jeremi360 <jbiernac@student.42warsaw.pl    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:56:52 by jeremi360         #+#    #+#             */
-/*   Updated: 2024/03/14 15:55:09 by jeremi360        ###   ########.fr       */
+/*   Updated: 2024/03/23 19:03:36 by jeremi360        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	is_space(char c)
+{
+	if (ft_strchr(" \t\n\v\f\r", c))
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -23,12 +30,30 @@ int	ft_atoi(const char *str)
 	nbr = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == '-')
-			sign *= -1;
-		if (ft_isdigit(str[i]))
+		if (ft_strchr("+-", str[i]))
+		{
+			if (str[i] == '-')
+			{
+				sign = -1;
+			}
+			if (is_space(str[i + 1]))
+				return (0);
+			if (ft_strchr("+-", str[i + 1]))
+				return (0);
+		}
+		else if (ft_isdigit(str[i]))
 		{
 			nbr = nbr * 10 + (str[i] - '0');
+			if (is_space(str[i + 1]))
+				return (sign * nbr);
 		}
+		else if (is_space(str[i]))
+		{
+			if (nbr != 0)
+				return (sign * nbr);
+		}
+		else
+			return (0);
 		i++;
 	}
 	return (sign * nbr);
