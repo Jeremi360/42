27d1c38c252917dfc930ead14df90193cf6528eb
Jeremi Biernacki <jeremi360@gmail.com>
2024-03-25 17:58:28 +0100

    norm fix

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jeremi360 <jbiernac@student.42warsaw.pl    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 18:57:59 by jeremi360         #+#    #+#             */
-/*   Updated: 2024/03/23 17:40:12 by jeremi360        ###   ########.fr       */
+/*   Updated: 2024/03/25 17:58:10 by jeremi360        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	if (!dst && !src)
 		return (NULL);
-
 	str1 = (unsigned char *)dst;
 	str2 = (unsigned char *)src;
 	while (n--)
