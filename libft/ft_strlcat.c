954ceee2ddafd8e752e2f0fd0104286ec26aca61
Jeremi Biernacki <jeremi360@gmail.com>
2024-04-08 16:17:25 +0200

    remove comments

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -22,10 +22,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dest_l = ft_strlen(dst);
 	if (dest_l >= size)
 		return (size + src_l);
-	// if (dest_l >= size)
-	// 	dest_l = size;
-	// if (dest_l == size)
-	// 	return (size + src_l);
 	if (src_l < size - dest_l)
 		ft_memcpy(dst + dest_l, src, src_l + 1);
 	else
