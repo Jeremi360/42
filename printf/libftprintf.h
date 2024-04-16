b1c5cfb1bfee16b0eeb31ee006acb3088d024988
Jeremi Biernacki <jeremi360@gmail.com>
2024-04-16 10:06:16 +0200

    lib

---
+++ printf/libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbiernac <jbiernac@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 09:51:37 by jbiernac          #+#    #+#             */
+/*   Updated: 2024/04/16 09:53:56 by jbiernac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include "libft/libft.h"
+
+int ft_printf(const char *, ...);
+
+#endif
+
