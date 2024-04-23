59aa28b9cf62e1b35102d5126a75978eac629152
Jeremi Biernacki <jeremi360@gmail.com>
2024-04-23 16:01:15 +0200

    too many vars

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jbiernac <jbiernac@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 09:45:57 by jbiernac          #+#    #+#             */
-/*   Updated: 2024/04/22 19:25:38 by jbiernac         ###   ########.fr       */
+/*   Updated: 2024/04/23 16:00:24 by jbiernac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,11 @@ static int search_for_tags(const char* tags, char *str, size_t len, char* tag_pt
 {
 	size_t i = 0;
 	char* tag;
-	char* tag_in_str;
 	while (tags[i] != 0)
 	{
 		tag = "%"+ tags[i];
-		tag_in_str = ft_strnstr(tag, str, len);
-		if (tag_in_str)
+		tag_ptr = ft_strnstr(tag, str, len);
+		if (tag_ptr)
 			return tags[i];
 		i++;
 	}
@@ -34,30 +33,29 @@ static void put_arg(int arg_type, va_list args)
 {
 	if (arg_type == 'c')
 		ft_putchar_fd(va_arg(args, int), 1);
-	else if (arg_type == 's')
+	if (arg_type == 's')
 		ft_putstr_fd(va_arg(args, char *), 1);
-	// else if (arg_type == 'p')
+	// if (arg_type == 'p')
 		//  ft_putnbr_fd(va_arg(args, pointer), 1);
-	// else if (arg_type == 'd')
+	// if (arg_type == 'd')
 		//  ft_putnbr_fd(va_arg(args, decimal), 1);
-	else if (arg_type == 'i')
+	if (arg_type == 'i')
 		ft_putnbr_fd(va_arg(args, int), 1);
-	// else if (arg_type == 'u')
+	// if (arg_type == 'u')
 		// ft_putnbr_fd(va_arg(args, unsigned decimal), 1);
-	// else if (arg_type == 'x')
+	// if (arg_type == 'x')
 		// ft_putnbr_fd(va_arg(args, hex low), 1);
-	// else if (arg_type == 'X')
+	// if (arg_type == 'X')
 		// ft_putnbr_fd(va_arg(args, hex up), 1);
-	else if (arg_type == '%')
+	if (arg_type == '%')
 		ft_putchar_fd('%', 1);
 }
 
 int ft_printf(const char *str, ...)
 {
 	const char* tags = "cspdiuxX%";
-	char *str_cpy = (char *) str;
 	char *str_dump;
-	char *found;
+	char *found = (char *) str;
 
 	va_list args;
 	va_start(args, str);
@@ -66,14 +64,13 @@ int ft_printf(const char *str, ...)
 	size_t t = 0;
 	size_t len = ft_strlen(str);
 
-	while (len > 0)
+	while (i < len)
 	{
-		t = search_for_tags(tags, str_cpy, len, found);
+		t = search_for_tags(tags, found, len, found);
 		if(t)
 		{
 			len = ft_strlen(found);
-			str_cpy = ft_strlen(found);
-			str_dump = ft_substr(str_cpy, 0, i);
+			str_dump = ft_substr(found, 0, i);
 			ft_putstr_fd(str_dump, 1);
 			free(str_dump);
 			put_arg(t, args);
