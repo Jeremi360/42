9cd5bd9137d86889b3e8cf7576dbf24e9b86fe8e
Jeremi Biernacki <jeremi360@gmail.com>
2024-04-29 13:51:02 +0200

    29.04 update

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jbiernac <jbiernac@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 09:45:57 by jbiernac          #+#    #+#             */
-/*   Updated: 2024/04/23 16:00:24 by jbiernac         ###   ########.fr       */
+/*   Updated: 2024/04/29 13:49:10 by jbiernac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,71 +14,84 @@
 # include <stdarg.h>
 # include "libftprintf.h"
 
-static int search_for_tags(const char* tags, char *str, size_t len, char* tag_ptr)
+static int search_for_tags(const char* tags, char c, char* tag_ptr)
 {
-	size_t i = 0;
-	char* tag;
-	while (tags[i] != 0)
+	printf("tag_to_find: %c\n", c);
+	tag_ptr = ft_strchr(tags, c);
+	if (tag_ptr)
 	{
-		tag = "%"+ tags[i];
-		tag_ptr = ft_strnstr(tag, str, len);
-		if (tag_ptr)
-			return tags[i];
-		i++;
+		printf("tag_ptr: %c\n", c);
+		return c;
 	}
 	return 0;
 }
 
-static void put_arg(int arg_type, va_list args) 
+static char * str_chr_dup(char c)
+{
+	char *temp = ft_strdup(" ");
+	temp[1] = c;
+	return temp;
+}
+
+static char* str_arg(int arg_type, va_list args)
 {
 	if (arg_type == 'c')
-		ft_putchar_fd(va_arg(args, int), 1);
+		return str_chr_dup(va_arg(args, int));
 	if (arg_type == 's')
-		ft_putstr_fd(va_arg(args, char *), 1);
+		return (va_arg(args, char *));
 	// if (arg_type == 'p')
-		//  ft_putnbr_fd(va_arg(args, pointer), 1);
+		//  return(va_arg(args, pointer), 1);
 	// if (arg_type == 'd')
-		//  ft_putnbr_fd(va_arg(args, decimal), 1);
+		//  return(va_arg(args, decimal), 1);
 	if (arg_type == 'i')
-		ft_putnbr_fd(va_arg(args, int), 1);
+		return(ft_itoa(va_arg(args, int)));
 	// if (arg_type == 'u')
-		// ft_putnbr_fd(va_arg(args, unsigned decimal), 1);
+		// return(va_arg(args, unsigned decimal), 1);
 	// if (arg_type == 'x')
-		// ft_putnbr_fd(va_arg(args, hex low), 1);
+		// return(va_arg(args, hex low), 1);
 	// if (arg_type == 'X')
-		// ft_putnbr_fd(va_arg(args, hex up), 1);
+		// return(va_arg(args, hex up), 1);
 	if (arg_type == '%')
-		ft_putchar_fd('%', 1);
+		return "%";
+	
+	return NULL;
 }
 
 int ft_printf(const char *str, ...)
 {
 	const char* tags = "cspdiuxX%";
-	char *str_dump;
 	char *found = (char *) str;
+	char *result = ft_strdup("");
 
 	va_list args;
 	va_start(args, str);
 
 	size_t i = 0;
-	size_t t = 0;
+	size_t tag = 0;
 	size_t len = ft_strlen(str);
 
 	while (i < len)
 	{
-		t = search_for_tags(tags, found, len, found);
-		if(t)
+		if (found[i] == '%')
 		{
-			len = ft_strlen(found);
-			str_dump = ft_substr(found, 0, i);
-			ft_putstr_fd(str_dump, 1);
-			free(str_dump);
-			put_arg(t, args);
+			i++;
+			tag = search_for_tags(tags, found[i], found);
+			if(tag != 0)
+			{
+				result = ft_strjoin(result, str_arg(tag, args));
+				printf("new_str: %s\n", result);
+			}
+		}
+		else
+		{
+			result += found[i];
+			printf("new_str: %s\n", result);
 		}
 		i++;
 	}
 
 	va_end(args);
+	ft_putstr_fd(result, 1);
 	return 0;
 }
 
@@ -86,45 +99,44 @@ int main()
 {
 	// %c
 	char test_chr = 'c';
-	printf("printf %%c: %c |", test_chr);
+	// printf("printf %%c: %c |", test_chr);
 	ft_printf("ft_printf %%c: %c |", test_chr);
 	
 	// %%
-	printf("printf %%%%: %% |");
+	// printf("printf %%%%: %% |");
 	ft_printf("ft_printf %%%%: %% |");
 
 	// %i
 	int test_num = 42;
-	printf("printf %%i: %i |", test_num);
-	// ft_printf("ft_printf %%i: %i |", test_num);
+	// printf("printf %%i: %i |", test_num);
+	ft_printf("ft_printf %%i: %i |", test_num);
 
 	// %s
 	char *test_str = "string";
-	printf("printf %%s: %s |", test_str);
-	// ft_printf("ft_printf %%s: %s |", test_str);
+	// printf("printf %%s: %s |", test_str);
+	ft_printf("ft_printf %%s: %s |", test_str);
 
 	// todo: %d
-	printf("printf %%d: %d |", test_num);
+	// printf("printf %%d: %d |", test_num);
 	// ft_printf("ft_printf %%d: %d |", test_num);
 
 	// todo: %p
-	char *ptr = "test";
-	printf("printf %%p: %p |", ptr);
+	// char *ptr = "test";
+	// printf("printf %%p: %p |", ptr);
 	// ft_printf("ft_printf %%p: %p |", ptr);
 
 	// todo: %u
-	printf("printf %%u: %u |", test_num);
+	// printf("printf %%u: %u |", test_num);
 	// ft_printf("ft_printf %%u: %u |", test_num);
 
 
 	// todo: %x
-	printf("printf %%x: %x |", test_num);
+	// printf("printf %%x: %x |", test_num);
 	// ft_printf("ft_printf %%x: %x |", test_num);
 
 	// todo: %X
-	printf("printf %%X: %X |", test_num);
+	// printf("printf %%X: %X |", test_num);
 	// ft_printf("ft_printf %%X: %X |", test_num);
-	ft_putstr_fd("\n", 1);
 
 	return 0;
 }
