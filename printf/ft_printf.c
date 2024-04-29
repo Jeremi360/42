b85dcfb10a1670a92084ab7ebfb9a056b04eea02
Jeremi Biernacki <jeremi360@gmail.com>
2024-04-29 14:30:46 +0200

    its base finall works :D

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jbiernac <jbiernac@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 09:45:57 by jbiernac          #+#    #+#             */
-/*   Updated: 2024/04/29 13:49:10 by jbiernac         ###   ########.fr       */
+/*   Updated: 2024/04/29 14:30:04 by jbiernac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@
 
 static int search_for_tags(const char* tags, char c, char* tag_ptr)
 {
-	printf("tag_to_find: %c\n", c);
 	tag_ptr = ft_strchr(tags, c);
 	if (tag_ptr)
 	{
-		printf("tag_ptr: %c\n", c);
 		return c;
 	}
 	return 0;
@@ -38,12 +36,10 @@ static char* str_arg(int arg_type, va_list args)
 	if (arg_type == 'c')
 		return str_chr_dup(va_arg(args, int));
 	if (arg_type == 's')
-		return (va_arg(args, char *));
+		return (ft_strdup(va_arg(args, char *)));
 	// if (arg_type == 'p')
 		//  return(va_arg(args, pointer), 1);
-	// if (arg_type == 'd')
-		//  return(va_arg(args, decimal), 1);
-	if (arg_type == 'i')
+	if (ft_strchr("id", arg_type))
 		return(ft_itoa(va_arg(args, int)));
 	// if (arg_type == 'u')
 		// return(va_arg(args, unsigned decimal), 1);
@@ -54,14 +50,14 @@ static char* str_arg(int arg_type, va_list args)
 	if (arg_type == '%')
 		return "%";
 	
-	return NULL;
+	return 0;
 }
 
 int ft_printf(const char *str, ...)
 {
 	const char* tags = "cspdiuxX%";
 	char *found = (char *) str;
-	char *result = ft_strdup("");
+	char *tmp = ft_strdup("");
 
 	va_list args;
 	va_start(args, str);
@@ -69,30 +65,31 @@ int ft_printf(const char *str, ...)
 	size_t i = 0;
 	size_t tag = 0;
 	size_t len = ft_strlen(str);
+	size_t print_lenght = 0;
 
 	while (i < len)
 	{
 		if (found[i] == '%')
 		{
-			i++;
-			tag = search_for_tags(tags, found[i], found);
+			tag = search_for_tags(tags, found[i+1], found);
 			if(tag != 0)
 			{
-				result = ft_strjoin(result, str_arg(tag, args));
-				printf("new_str: %s\n", result);
+				tmp = str_arg(tag, args);
+				print_lenght += ft_strlen(tmp);
+				ft_putstr_fd(tmp, 1);
+				i++;
 			}
 		}
 		else
 		{
-			result += found[i];
-			printf("new_str: %s\n", result);
+			print_lenght++;
+			ft_putchar_fd(found[i], 1);
 		}
 		i++;
 	}
 
 	va_end(args);
-	ft_putstr_fd(result, 1);
-	return 0;
+	return print_lenght;
 }
 
 int main()
