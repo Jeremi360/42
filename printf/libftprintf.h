c5e80b254354c872533386202093a93d72fc4852
Jeremi Biernacki <jeremi360@gmail.com>
2024-05-11 12:01:48 +0200

    move libft files to root folder for makefile to work

--- printf/libftprintf.h
+++ printf/libftprintf.h
@@ -6,14 +6,14 @@
 /*   By: jbiernac <jbiernac@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 09:51:37 by jbiernac          #+#    #+#             */
-/*   Updated: 2024/05/07 15:27:34 by jbiernac         ###   ########.fr       */
+/*   Updated: 2024/05/11 12:00:31 by jbiernac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-# include "libft/libft.h"
+# include "libft.h"
 
 int ft_printf(const char *, ...);
 char*	ft_hex(unsigned long nb, char format);
